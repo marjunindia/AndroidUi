7bc27ea4ff24297907a062b839e76fa9b1d6d034
arjunarjun27 <arjunarjun27@gmail.com>
2018-04-28 20:10:34 +0530

    string format,textscrolling

--- app/src/main/java/com/example/arjun27/androidui/MainActivity.java
+++ app/src/main/java/com/example/arjun27/androidui/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.arjun27.androidui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView=(TextView)findViewById(R.id.mytext);
+        String mytext=String.format(getString(R.string.hello),"my world");
+        textView.setText(mytext);
+
+
+        TextView textView1=(TextView)findViewById(R.id.textView1);
+        textView1.setMovementMethod(new ScrollingMovementMethod());
+
     }
 }
