035a92916091e0d45b73096aa6760d2e59c3b4e8
arjunarjun27 <arjunarjun27@gmail.com>
2018-04-29 15:24:48 +0530

    imageview,picasso

--- app/src/main/java/com/example/arjun27/androidui/MainActivity.java
+++ app/src/main/java/com/example/arjun27/androidui/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.arjun27.androidui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView imageView=(ImageView)findViewById(R.id.imageView2);
+
+        Picasso.get().load(R.drawable.screenshot).into(imageView);
+
     }
 }
